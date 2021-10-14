9f9f191a9f6d13cdf4b867f7d70a749cbb0ee67f
VarunIrani <varunirani0505@gmail.com>
2021-10-14 16:02:28 +0530

    Fixed bugs 🪲. Resume Testing 🧪

--- src/main/java/com/stqa/astar/cell/Cell.java
+++ src/main/java/com/stqa/astar/cell/Cell.java
@@ -1,61 +1,107 @@
 package com.stqa.astar.cell;
 
-import com.stqa.astar.grid.Grid;
+import com.stqa.astar.AStar;
 
 import java.awt.*;
 import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Cell {
+	private final int x;
+	private final int y;
+	public static int w, h;
+	private int fCost;
+	private int gCost;
+	private int hCost;
 	public final int i, j;
-	private int fCost, gCost, hCost;
 	private final ArrayList<Cell> neighbors = new ArrayList<>();
-	public Cell previous;
-	private boolean isWall, isStart, isEnd;
-	public final int x, y;
-	public static int width, height;
-	public Color color;
+	private Cell previous = null;
+	private boolean wall, start, end;
 	private static final HashMap<Point, int[]> pointMap = new HashMap<>();
-	
+	public Color color;
+
+	private final int rows = 30;
+	private final int cols = 30;
+
+	public ArrayList<Cell> getNeighbors() {
+		return neighbors;
+	}
+
 	public Cell(int i, int j) {
 		this.i = i;
 		this.j = j;
 		this.fCost = 0;
 		this.gCost = 0;
 		this.hCost = 0;
-		this.isWall = false;
-		this.isStart = false;
-		this.isEnd = false;
-		width = Grid.width / Grid.rows - 10;
-		height = Grid.width / Grid.cols - 10;
-		this.x = i * width + 5;
-		this.y = j * height + 5;
-		this.color = Color.WHITE;
-		
-		int[] c = { i, j };
-		for (int k = x; k < x + width; k++)
-			for (int l = y; l < y + height; l++)
+		this.wall = false;
+		this.start = false;
+		this.end = false;
+
+		int width = AStar.width;
+
+		w = width / rows;
+		h = width / cols;
+
+		w *= 0.55;
+		h *= 0.55;
+
+		this.x = i * w;
+		this.y = j * h;
+
+		int[] c = {i, j};
+		for (int k = x; k < x + w; k++)
+			for (int l = y; l < y + h; l++)
 				pointMap.put(new Point(k, l), c);
+
+		color = Color.WHITE;
 	}
 
-	public void draw(Graphics g) {
-		Graphics2D g2 = (Graphics2D) g;
-		Stroke s = new BasicStroke(2f);
-		g2.setStroke(s);
-		g2.setColor(color);
-		g2.fillRect(x, y, width, height);
-		g2.setColor(Color.BLACK);
-		g2.drawRect(x, y, width, height);
- 	}
-	
+
+	public boolean isWall() {
+		return wall;
+	}
+
+	public boolean isStart() {
+		return start;
+	}
+
+	public boolean isEnd() {
+		return end;
+	}
+
+	public void setWall(boolean wall) {
+		this.wall = wall;
+	}
+
+	public void setStart(boolean start) {
+		this.start = start;
+	}
+
+	public void setEnd(boolean end) {
+		this.end = end;
+	}
+
+	/**
+	 * @return int[]
+	 */
 	public static int[] clickedCell(Point click) {
 		return pointMap.get(click);
 	}
- 
-	public void addNeighbors(Cell[][] grid, int rows, int cols) {
+
+	/**
+	 * @return Point
+	 */
+	public Point getPoint() {
+		return new Point(i * w, j * h);
+	}
+
+	/**
+	 *
+	 */
+	public void addNeighbors(Cell[][] grid) {
 		int i = this.i;
 		int j = this.j;
-		// * Non-Diagonals
+
 		if (i < cols - 1) {
 			this.neighbors.add(grid[i + 1][j]);
 		}
@@ -83,8 +129,21 @@ public class Cell {
 		}
 	}
 
-	public ArrayList<Cell> getNeighbors() {
-		return neighbors;
+	/**
+	 *
+	 */
+	public void draw(Graphics g_) {
+		Graphics2D g = (Graphics2D) g_;
+		Stroke s = new BasicStroke(2f);
+		g.setStroke(s);
+		g.setColor(this.color);
+		g.fillRect(x, y, w, h);
+		if (this.wall) {
+			g.setColor(new Color(50, 50, 50));
+			g.fillRect(x, y, w, h);
+		}
+		g.setColor(Color.BLACK);
+		g.drawRect(x, y, w, h);
 	}
 
 	public int getFCost() {
@@ -111,35 +170,11 @@ public class Cell {
 		this.hCost = hCost;
 	}
 
-	public boolean isWall() {
-		return isWall;
-	}
-
-	public void setWall(boolean wall) {
-		isWall = wall;
-	}
-
-	public boolean isStart() {
-		return isStart;
-	}
-
-	public void setStart(boolean start) {
-		isStart = start;
-	}
-
-	public boolean isEnd() {
-		return isEnd;
-	}
-
-	public void setEnd(boolean end) {
-		isEnd = end;
+	public void setPrevious(Cell previous) {
+		this.previous = previous;
 	}
 
 	public Cell getPrevious() {
 		return previous;
-	}
-
-	public void setPrevious(Cell previous) {
-		this.previous = previous;
 	}
 }
--- src/main/java/com/stqa/astar/grid/Grid.java
+++ src/main/java/com/stqa/astar/grid/Grid.java
@@ -5,20 +5,17 @@ import com.stqa.astar.cell.Cell;
 import com.stqa.astar.core.AStarCore;
 
 import java.awt.*;
-import java.util.HashMap;
-import java.util.Random;
 
 public class Grid {
 	public static int width, height;
-	public static final int rows = 10, cols = 10;
+	public static final int rows = 30, cols = 30;
 	private static final Cell[][] grid = new Cell[rows][cols];
 	private static Cell start;
 	private static Cell end;
 
 	public Grid() {
-		Grid.width = (int) (AStar.width * 0.40);
+		Grid.width = AStar.width;
 		Grid.height = AStar.height;
-		
 		this.setup();
 	}
 
@@ -28,23 +25,22 @@ public class Grid {
 				grid[i][j] = new Cell(i, j);
 			}
 		}
-		
+
 		for (int i = 0; i < rows; i++) {
 			for (int j = 0; j < cols; j++) {
-				grid[i][j].addNeighbors(grid, rows, cols);
+				grid[i][j].addNeighbors(grid);
 			}
 		}
-		
 	}
 
-	public void drawAllCell(Graphics graphics) {
+	public void drawAllCells(Graphics graphics) {
 		for (Cell[] cells : grid) {
 			for (Cell cell : cells) {
 				cell.draw(graphics);
 			}
 		}
 	}
-	
+
 	public static Cell[][] getGrid() {
 		return grid;
 	}
@@ -52,11 +48,11 @@ public class Grid {
 	public static Cell getStart() {
 		return start;
 	}
-	
+
 	public static Cell getEnd() {
 		return end;
 	}
-	
+
 	public void setStart(int i, int j) {
 		start = grid[i][j];
 		AStarCore.openSet.add(start);
@@ -65,31 +61,25 @@ public class Grid {
 	}
 
 	public void setEnd(int i, int j) {
-		System.out.println("(" + i + "," + j + ")");
 		end = grid[i][j];
 		grid[i][j].color = new Color(255, 94, 90);
 		grid[i][j].setEnd(true);
 	}
-	
+
 	public void setWall(int i, int j) {
 		grid[i][j].setWall(true);
 		grid[i][j].color = new Color(50, 50, 50);
 	}
-	
-	public void removeTheWall(int i, int j) {
-		grid[i][j].setWall(false);
-		grid[i][j].color = Color.WHITE;
+
+	public boolean isNotWall(int i, int j) {
+		return !grid[i][j].isWall();
 	}
-	
-	public boolean isWall(int i, int j) {
-		return grid[i][j].isWall();
+
+	public boolean isNotStart(int i, int j) {
+		return !grid[i][j].isStart();
 	}
-	
-	public boolean isStart(int i, int j) {
-		return grid[i][j].isStart();
-	}
-	
-	public boolean isEnd(int i, int j) {
-		return grid[i][j].isEnd();
+
+	public boolean isNotEnd(int i, int j) {
+		return !grid[i][j].isEnd();
 	}
 }
